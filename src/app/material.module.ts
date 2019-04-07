eb9ffb016c7600c20e6654d5fd9c7899c3810fc3
rvktmr@gmail.com <rvktmr@gmail.com>
2019-04-07 22:30:54 +0300

    Material workshop

---
+++ src/app/material.module.ts
@@ -0,0 +1,36 @@
+import {NgModule} from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatDialogModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSlideToggleModule,
+  MatTabsModule,
+  MatToolbarModule
+} from '@angular/material';
+
+@NgModule({
+  exports:[
+    MatButtonModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatDialogModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSlideToggleModule,
+  MatTabsModule,
+  MatToolbarModule,
+  FlexLayoutModule
+  ]
+})export class AppMaterialModule {}
